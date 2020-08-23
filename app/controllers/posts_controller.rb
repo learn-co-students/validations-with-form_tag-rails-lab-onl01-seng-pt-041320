3f4502bce67aa3068ff726fb7aad138288a59fa3
Dice-K39 <daisukehashimoto39@gmail.com>
2020-08-23 10:23:19 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,17 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
+    # @post = Post.find(params[:id])
+
+    # @post.update(post_params)
+
+    # redirect_to post_path(@post)
   end
 
   private

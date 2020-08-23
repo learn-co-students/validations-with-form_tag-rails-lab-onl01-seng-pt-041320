4cede925cea72cd8e4a867a2591f899ac6e29c36
OrkunSA <c.orkun.s@gmail.com>
2020-08-23 18:38:58 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
-class Author < ActiveRecord::Base
-end
+  class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :phone_number, length: {is: 10}
+  end

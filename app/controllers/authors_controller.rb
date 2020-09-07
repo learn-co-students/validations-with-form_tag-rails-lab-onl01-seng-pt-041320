2a515cd15d416bd2628e2d8bab21bb2f9fac423a
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-07 12:54:19 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author  
   end
 
   def new
@@ -8,14 +8,35 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    # binding.pry
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+      if @author.valid?
+        # @author.save
+        redirect_to author_path(@author)
+      else
+        render :new
+      end
   end
 
-  private
+  def edit
+    set_author
+  end
 
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
+
+  private
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
